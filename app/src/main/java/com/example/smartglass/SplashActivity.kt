34626ad398e0512281f566912050da1836ac9ca7
KZero27 <phamanhkhoazero27@gmail.com>
2025-08-05 23:24:48 +0700

    Thay doi code Splash Screen

---
+++ app/src/main/java/com/example/smartglass/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.yourapp
+import com.example.smartglass.R
+import com.example.smartglass.MainActivity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000) // 3000 milliseconds = 3 seconds
+    }
+}
