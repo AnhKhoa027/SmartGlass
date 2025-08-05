1c8841657710e6859d5d73d4dc12a9dea610c3a2
KZero27 <phamanhkhoazero27@gmail.com>
2025-08-06 01:25:04 +0700

    Anh Khoa: Update Bottom Navigation

--- app/src/main/java/com/example/smartglass/MainActivity.kt
+++ app/src/main/java/com/example/smartglass/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Set fragment mặc định khi mở app
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, HomeFragment())
+            .commit()
+
+        // Xử lý bottom navigation
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val selectedFragment: Fragment = when (item.itemId) {
+                R.id.home -> HomeFragment()
+                R.id.setting -> SettingFragment()
+                else -> HomeFragment()
+            }
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, selectedFragment)
+                .commit()
+
+            true
         }
     }
 }
